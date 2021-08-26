8f8d0743cde41f02334ee59affe1d9900fce003c
wcgordon1 <85032863+wcgordon1@users.noreply.github.com>
2021-08-25 21:52:33 -0700

    docs

---
+++ Docs/Guess-Number/script.js
@@ -0,0 +1,11 @@
+let computerGuess
+function init() {
+    computerGuess = Math.floor(Math.random() * 100 +1);
+    document .getElementById("newGameButton") .style.display = "none";
+    document . getElementById("gameArea") .style.display = "none";
+}
+
+function startGameView() {
+    document .getElementById("welcomeScreen") .style.display = "none";
+    document .getElementById("gameArea") .style.display = "block";
+} 
